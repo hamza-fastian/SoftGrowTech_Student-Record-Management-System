e70de75d53c0b91a6deccfe633867a49d10277b0
hamza-fastian <h00231755@gmail.com>
2026-05-24 09:26:02 +0500

    Add files via upload

---
+++ Project5.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//#include<fstream>
+//#include<string>
+//using namespace std;
+//
+//int main()
+//{
+//	int occurence = 0;
+//	ifstream in("file.txt");
+//	string word;
+//	string ch;
+//	cout << "Enter a word: ";
+//	cin >> word;
+//	while (in >> ch)
+//	{
+//		if (ch == word)
+//		{
+//			occurence++;
+//		}
+//	}
+//	if (occurence > 0)
+//	{
+//		cout << "Word occured in file for " << occurence << " times" << endl;
+//	}
+//	else
+//	{
+//		cout << "Word not found!" << endl;
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
